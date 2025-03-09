079dd31e9c17b2a75015856c8399e420021ce3b8
Kaike Bartolomeu <102388705+Kaike2003@users.noreply.github.com>
2025-03-09 16:08:43 +0100

    v1 terminado

---
+++ src/@types/provincias.ts
@@ -0,0 +1,10 @@
+export type TProvincias = {
+  id?: string;
+  nome?: string;
+  fundada?: string;
+  capital?: string;
+  area?: string;
+  prefixo_telefonico?: string;
+  site_governo_provincial?: string;
+  municipios?: Array<string>;
+};
